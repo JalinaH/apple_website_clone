1f9c6f4688a2e933edd0c39e5dd062cf69caa983
Jalina Hirushan <gunathungagajh.22@uom.lk>
2024-06-27 22:08:10 +0530

    chore: Update favicon and page title

--- src/components/Highlights.jsx
+++ src/components/Highlights.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 function Highlights() {
   return (
-    <div>
-      
-    </div>
+    <section id='highlights' className='w-screen overflow-hidden'>
+
+    </section>
   )
 }
 
