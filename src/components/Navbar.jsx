14b9062e866a0f6e51c5c5f90a27810b9260b9e7
Jalina Hirushan <gunathungagajh.22@uom.lk>
2024-06-27 20:50:40 +0530

    chore: Update favicon and page title

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { appleImg } from "../utils";
+
+function Navbar() {
+  return (
+    <header>
+      <nav>
+        <img src={appleImg} alt="Apple" width={14} height={18} />
+
+        <div>
+            
+        </div>
+      </nav>
+    </header>
+  );
+}
+
+export default Navbar;
